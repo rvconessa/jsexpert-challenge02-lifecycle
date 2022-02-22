83f77b04b0087eeda6f262a9b3b7609154818e93
Renan <rvconessa@gmail.com>
2022-02-22 15:56:09 -0300

    test: add ObjectMethods Test

--- src/service/ObjectMethodsService.js
+++ src/service/ObjectMethodsService.js
@@ -1,18 +1,38 @@
 class ObjectMethodsService {
   getEnhancedObject(rawObject) {
     // TODO: return an enhanced object implementing `valueOf` or `toString` depending on the context
-    return null;
+    return {
+      ...rawObject, 
+      valueOf(){ 
+        return rawObject.age 
+      }
+    }
   }
 
   getEnhancedObject2(rawObject) {
     // TODO: return an enhanced object implementing `valueOf` or `toString` depending on the context
-    return null;
+    return {
+      ...rawObject, 
+      toString(){ 
+        return `[name="${rawObject.name}",age=${rawObject.age}]`
+      }
+    }
   }
 
   getEnhancedObjectWithoutValueOfOrToString(rawObject) {
     // TODO: return an enhanced object **WITHOUT** implementing `valueOf` nor `toString`.
     // Note: Implementing `toString` and `valueOf` will NOT be allowed in this function.
-    return null;
+    return {
+      ...rawObject,
+      [Symbol.toPrimitive](coercionType) {
+        const types = {
+          string: `[name="${rawObject.name}",age=${rawObject.age}]`,
+          number: rawObject.age 
+        }
+
+        return types[coercionType]
+      }
+    }
   }
 }
 
